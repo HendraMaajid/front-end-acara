747b876ff62188781bae262b78d9c8d3b8eb5066
HendraMaajid <hendralatiefulm@gmail.com>
2025-07-08 21:08:16 +0700

    setup environment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import { Card, CardHeader, CardBody, CardFooter, Divider, Link, Image } from "@heroui/react";
+import PageHead from "@/components/commons/PageHead";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,31 +17,7 @@ export default function Home() {
     <div
       className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 dark font-[family-name:var(--font-geist-sans)]`}
     >
-      <Card className="max-w-[400px] mt-20">
-        <CardHeader className="flex gap-3">
-          <Image
-            alt="heroui logo"
-            height={40}
-            radius="sm"
-            src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
-            width={40}
-          />
-          <div className="flex flex-col">
-            <p className="text-md">HeroUI</p>
-            <p className="text-small text-default-500">heroui.com</p>
-          </div>
-        </CardHeader>
-        <Divider />
-        <CardBody>
-          <p>Make beautiful websites regardless of your design experience.</p>
-        </CardBody>
-        <Divider />
-        <CardFooter>
-          <Link isExternal showAnchorIcon href="https://github.com/heroui-inc/heroui">
-            Visit source code on GitHub.
-          </Link>
-        </CardFooter>
-      </Card>
+      <PageHead title="Home" />
     </div>
   );
 }
